66a63d03b69d76945b361d1fddf53a348c50da41
Caio Gomes <caiogomes34@gmail.com>
2018-04-08 22:21:49 -0300

    Updated the file to make a simple object move test.

--- bootloader/Bootloader_template/kernel.asm
+++ bootloader/Bootloader_template/kernel.asm
@@ -100,48 +100,7 @@ start:
 	
 	mov [barY1], word 50
 	mov [barY2], word 50
-
-	mov al, white
-	;; Draw the first bar
-	mov dx, word [barX1]
-	mov [objectX], dx
-	mov dx, word [barY1]
-	mov [objectY], dx
-	mov dx, word [barHeight]
-	mov [objectHeight], dx
-	mov dx, word [barWidth]
-	mov [objectWidth], dx
-	call draw_rectangle
-
-	;; Draw the second bar
-	mov dx, word [barX2]
-	mov [objectX], dx
-	mov dx, word [barY2]
-	mov [objectY], dx
-	mov dx, word [barHeight]
-	mov [objectHeight], dx
-	mov dx, word [barWidth]
-	mov [objectWidth], dx
-	call draw_rectangle
-
-	;; Draw the ball
-	mov dx, word [ballX]
-	mov [objectX], dx
-	mov dx, word [ballY]
-	mov [objectY], dx
-	mov dx, word [ballSize]
-	mov [objectHeight], dx
-	mov dx, word [ballSize]
-	mov [objectWidth], dx
-	call draw_rectangle
-
-	;; Setup the color and the page
-	; mov bh, 0
-	; mov bl, light_green
-
-	; mov si, hello
-	; call print_data_seg
-
+	jmp game_loop
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Subrountines            ;;
@@ -150,6 +109,27 @@ game_loop:
 	;; Must draw every rectangle in the screen
 	;; pass the function params in the stack and use a register to store the 
 	;; return addres
+	
+	mov dx, word [barY1]
+	loop_toprint:
+		cmp dx, 0
+		je end_loop_toprint
+		
+		push dx
+		mov al, black
+		call print_bar_1
+		pop dx
+		
+		dec dx
+		mov [barY1], dx
+		push dx
+
+		mov al, white
+		call print_board
+		pop dx
+		
+		jmp loop_toprint
+	end_loop_toprint:
 
 	
 
@@ -243,67 +223,59 @@ draw_rectangle:
 	end_draw_rectangle_loop:
 	ret
 
-;; Store at si the value to print
-print_num:
-	push ax
-	push bx
-	push cx
-	push dx
-	mov bp, sp
-
-	mov ax, si
-	mov si, sp
-	call int_to_char
-	call print_si_to_sp
-
-	mov sp, bp
-	pop dx
-	pop cx
-	pop bx
-	pop ax
+print_board:
+	mov al, white
+	call print_game_elements
 	ret
 
-;; Put the integer in cx and we use all the 4 registers
-;; Must store in si the top of the stack
-;; mov to ax the value you want to store
-int_to_char:
-	call setup_div
-	;; Store the return value
-	pop cx
-	mov bx, 10
-	convert_int_to_char:
-		div bx
-		add dx, '0'
-		push dx
-		je end_convert_int_to_char
-		jmp convert_int_to_char
-	end_convert_int_to_char:
-		ret
-
-setup_div:
-	xor ax, ax
-	xor dx, dx
-	mov ax, cx
+delete_board:
+	mov al, black
+	call print_game_elements
 	ret
 
-;; We use the reg cx and bx
-print_si_to_sp:
-	;; Setup the color and the page
-	mov bh, 0
-	mov bl, light_green
-	
-	mov cx, si
-	loop_print_si_to_sp:
-		cmp si, sp
-		je end_loop_print_si_to_sp
-		mov ax, [si]
-		call print_char
-		dec si
-		jmp loop_print_si_to_sp
-	end_loop_print_si_to_sp:
-	mov si, cx
-	;; Reset the sp
-	mov sp, si
+print_game_elements:
+	call print_bar_1
+	call print_bar_2
+	call print_ball
+	ret
+
+print_bar_1:
+	;; Draw the first bar
+	mov dx, word [barX1]
+	mov [objectX], dx
+	mov dx, word [barY1]
+	mov [objectY], dx
+	mov dx, word [barHeight]
+	mov [objectHeight], dx
+	mov dx, word [barWidth]
+	mov [objectWidth], dx
+	call draw_rectangle
+	ret
+
+print_bar_2:
+	;; Draw the second bar
+	mov dx, word [barX2]
+	mov [objectX], dx
+	mov dx, word [barY2]
+	mov [objectY], dx
+	mov dx, word [barHeight]
+	mov [objectHeight], dx
+	mov dx, word [barWidth]
+	mov [objectWidth], dx
+	call draw_rectangle
+	ret
+
+print_ball:
+	;; Draw the ball
+	mov dx, word [ballX]
+	mov [objectX], dx
+	mov dx, word [ballY]
+	mov [objectY], dx
+	mov dx, word [ballSize]
+	mov [objectHeight], dx
+	mov dx, word [ballSize]
+	mov [objectWidth], dx
+	call draw_rectangle
 	ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
