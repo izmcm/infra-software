64425462b65e01326b99101980f4f692f0ed289d
Caio Gomes <caiogomes34@gmail.com>
2018-04-08 22:06:23 -0300

    Setting up the game draw methods to create the engine.

--- bootloader/Bootloader_template/kernel.asm
+++ bootloader/Bootloader_template/kernel.asm
@@ -82,7 +82,13 @@ org 0x7e00
 
 bits 16
 
-jmp start
+jmp setup
+
+setup:
+	push bp
+	mov bp, sp
+	call clean_all
+	jmp start
 
 start:
 	xor ax, ax
@@ -91,23 +97,50 @@ start:
 
 	call set_video_mode
 	
-	mov ax, green
-	mov si, objectX
-	mov [si], 0
-	mov si, objectY
-	mov [si], 0
-	mov si, objectHeight
-	mov [si], 100
-	mov si, objectWidth
-	mov [si], 200
-	call draw_square
+	
+	mov [barY1], word 50
+	mov [barY2], word 50
+
+	mov al, white
+	;; Draw the first bar
+	mov dx, word [barX1]
+	mov [objectX], dx
+	mov dx, word [barY1]
+	mov [objectY], dx
+	mov dx, word [barHeight]
+	mov [objectHeight], dx
+	mov dx, word [barWidth]
+	mov [objectWidth], dx
+	call draw_rectangle
+
+	;; Draw the second bar
+	mov dx, word [barX2]
+	mov [objectX], dx
+	mov dx, word [barY2]
+	mov [objectY], dx
+	mov dx, word [barHeight]
+	mov [objectHeight], dx
+	mov dx, word [barWidth]
+	mov [objectWidth], dx
+	call draw_rectangle
+
+	;; Draw the ball
+	mov dx, word [ballX]
+	mov [objectX], dx
+	mov dx, word [ballY]
+	mov [objectY], dx
+	mov dx, word [ballSize]
+	mov [objectHeight], dx
+	mov dx, word [ballSize]
+	mov [objectWidth], dx
+	call draw_rectangle
 
 	;; Setup the color and the page
-	mov bh, 0
-	mov bl, light_green
+	; mov bh, 0
+	; mov bl, light_green
 
-	mov si, hello
-	call print_data_seg
+	; mov si, hello
+	; call print_data_seg
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -174,63 +207,134 @@ draw_rectangle:
 	;; Uses all the four registers
 	;; Move to the global variables objectX, objectY, objectHeight, objectWidth
 	;; The dimensions of the rectangle wanted
-
 	xor dx, dx
 	xor cx, cx
 	xor bx, bx
 
 	mov ah, pixel_service
 	;; Size of the rectangle
-	mov si, objectWidth
-	mov cx, [si]
-	mov si, objectHeight
-	mov dx, [si]
+	mov cx, word [objectX]
+	mov dx, word [objectY]
 	
 	;; Put in the center of the screen
-	mov si, objectX
-	add cx, [si]
-	mov si, objectY
-	add dx, [si]
+	add cx, word [objectWidth]
+	add dx, word [objectHeight]
 
 	draw_rectangle_loop:
 		dec cx
-		mov si, objectX
-		cmp cx, [si]
+		cmp cx, word [objectX]
+
 		je partial_draw_rectangle_loop
 		
 		int 10h
 		jmp draw_rectangle_loop
 		partial_draw_rectangle_loop:
-			mov cx, square_size 
-			add cx, square_x
+			;; Reposition the cursor of X
+			mov cx, word [objectX]
+			add cx, word [objectWidth]
+
+			mov si, dx
+			; call print_num
 
 			dec dx
-			mov si, objectY
-			cmp dx, [si]
-			je end_draw_rectangle_loop
+			cmp dx, word [objectY]
+			jle end_draw_rectangle_loop
 			jmp draw_rectangle_loop
 	end_draw_rectangle_loop:
 	ret
 
+;; Store at si the value to print
+print_num:
+	push ax
+	push bx
+	push cx
+	push dx
+	mov bp, sp
+
+	mov ax, si
+	mov si, sp
+	call int_to_char
+	call print_si_to_sp
+
+	mov sp, bp
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	ret
+
+;; Put the integer in cx and we use all the 4 registers
+;; Must store in si the top of the stack
+;; mov to ax the value you want to store
+int_to_char:
+	call setup_div
+	;; Store the return value
+	pop cx
+	mov bx, 10
+	convert_int_to_char:
+		div bx
+		add dx, '0'
+		push dx
+		je end_convert_int_to_char
+		jmp convert_int_to_char
+	end_convert_int_to_char:
+		ret
+
+setup_div:
+	xor ax, ax
+	xor dx, dx
+	mov ax, cx
+	ret
+
+;; We use the reg cx and bx
+print_si_to_sp:
+	;; Setup the color and the page
+	mov bh, 0
+	mov bl, light_green
+	
+	mov cx, si
+	loop_print_si_to_sp:
+		cmp si, sp
+		je end_loop_print_si_to_sp
+		mov ax, [si]
+		call print_char
+		dec si
+		jmp loop_print_si_to_sp
+	end_loop_print_si_to_sp:
+	mov si, cx
+	;; Reset the sp
+	mov sp, si
+	ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Messages                ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-msg1: db 'Testing the kernel...', carriage_return, endl, 0
-hello: db 'Hello Izabella I am trying to print in the video mode!', carriage_return, endl, 0
+msg1: dw 'Testing the kernel...', carriage_return, endl, 0
+hello: dw 'Hello Izabella I am trying to print in the video mode!', carriage_return, endl, 0
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Global Variables        ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-objectWidth: db 0
-objectHeight: db 0
-objectX: db 0
-objectY: db 0
+objectWidth: dw 0
+objectHeight: dw 0
+objectX: dw 0
+objectY: dw 0
+
+barY1: dw 0
+barX1: dw 0
+barY2: dw 0
+barX2: dw 300
+barHeight: dw 60
+barWidth: dw 10
+
+ballX: dw 150
+ballY: dw 94
+ballSize: dw 10
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Boot Signature          ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 exit:
-	times 510-($-$$) db 0 ;512 bytes
+	times 510-($-$$) dw 0 ;512 bytes
 	dw 0xaa55             ;assinatura
